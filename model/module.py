c76f929a152cf667b9d3763ecd3b4ea0fce5c842
saeedahmadicp <saeedahmad.icp@gmail.com>
2023-07-31 17:22:34 +0900

    added double conv layer module

---
+++ model/module.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+__all__ = ['DoubleConv']
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        
+        self.conv = nn.Sequential(
+            ## 1st conv layer
+            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.GroupNorm(num_groups=out_channels//8, num_channels=out_channels),
+            nn.LeakyReLU(inplace=True, negative_slope=0.01),
+            
+            ## 2nd conv layer
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.GroupNorm(num_groups=out_channels//8, num_channels=out_channels),
+            nn.LeakyReLU(inplace=True, negative_slope=0.01),
+        )
+        
+    def forward(self, x):
+        return self.conv(x)
