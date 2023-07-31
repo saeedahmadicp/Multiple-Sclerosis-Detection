519488f46a1f2d169c2e5ab4475e692eeeb0d329
saeedahmadicp <saeedahmad.icp@gmail.com>
2023-07-31 17:38:58 +0900

    added encoder, updated module.py

---
+++ model/encoder.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+from module import DoubleConv
+
+__all__ = ['Encoder']
+class Encoder(nn.Module):
+    def __init__(self, in_channels, out_channels, filters = [32, 64, 128, 256, 512]):
+        super(Encoder, self).__init__() 
+        
+        self.dconv_down1 = DoubleConv(in_channels, filters[0], stride=1)
+        self.dconv_down2 = DoubleConv(filters[0], filters[1], stride=2)
+        self.dconv_down3 = DoubleConv(filters[1], filters[2], stride=2)
+        self.dconv_down4 = DoubleConv(filters[2], filters[3], stride=2)
+        self.dconv_down5 = DoubleConv(filters[3], filters[4], stride=2)
+        
+    def forward(self, x):
+        x1 = self.dconv_down1(x) # 32
+        x2 = self.dconv_down2(x1) # 64
+        x3 = self.dconv_down3(x2) # 128
+        x4 = self.dconv_down4(x3) # 256
+        x5 = self.dconv_down5(x4) # 512
+        
+        return x1, x2, x3, x4, x5
+        
+        
--- model/module.py
+++ model/module.py
@@ -4,17 +4,17 @@ import torch.nn as nn
 __all__ = ['DoubleConv']
 
 class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, stride=1):
         super(DoubleConv, self).__init__()
         
         self.conv = nn.Sequential(
             ## 1st conv layer
-            nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.Conv3d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
             nn.GroupNorm(num_groups=out_channels//8, num_channels=out_channels),
             nn.LeakyReLU(inplace=True, negative_slope=0.01),
             
             ## 2nd conv layer
-            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
             nn.GroupNorm(num_groups=out_channels//8, num_channels=out_channels),
             nn.LeakyReLU(inplace=True, negative_slope=0.01),
         )
