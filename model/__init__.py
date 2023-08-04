ee1101d246d29a99a69d1e9e526a12a7f2f3b70a
saeedahmadicp <saeedahmad.icp@gmail.com>
2023-08-05 00:21:59 +0900

    code refactor

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,5 @@
 from .decoder import Decoder
 from .encoder import Encoder
+from .classifier import SclerosisClassifier
 
-__all__ = ['Encoder', 'Decoder']
+__all__ = ['Encoder', 'Decoder', 'SclerosisClassifier']
